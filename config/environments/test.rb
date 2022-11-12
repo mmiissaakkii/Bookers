866ea4039ecf114716ef6155bc049f805ebfc3c4
EC2 Default User <ec2-user@ip-172-31-8-163.ap-northeast-1.compute.internal>
2022-11-12 10:47:02 +0000

    bookers

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,10 +44,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :silence
 
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
+
 
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
@@ -57,4 +58,5 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
 end
